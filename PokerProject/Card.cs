b9f2d373d25b636f879a203fef88bb5fb1efaff4
Swathi Chakkilam <swathichakkilam@gmail.com>
2018-10-24 21:37:24 -0700

    Poker project - Card Class and Program

---
+++ PokerProject/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerProject
+{
+    class Card
+    {
+        public enum Suit
+        {
+            HEARTS,
+            SPADES,
+            DIAMONDS,
+            CLUBS
+        }
+        public enum CardValue
+        {
+            one,two,three,four,five,six,seven,
+            eight,nine,ten,Jack,Queen,King,Ace
+        }
+        #region Card Properties
+        /// <summary>
+        /// CurrSuitVal is a property of type Suit which is an enum 
+        /// where CurrSuitVal is a way to retrieve the current suit value and store it in CurrSuitVal
+        /// </summary>
+        public Suit CurrSuitVal { get; set; }
+
+        /// <summary>
+        /// CurrentCardVal is a property of type CardValue which is an enum
+        /// where CurrentCardVal holds the current card value at that moment 
+        /// </summary>
+        //used camel casing for property names for readability 
+       public CardValue CurrentCardVal { get; set; }
+        #endregion
+
+
+
+    }
+}
---
+++ PokerProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PokerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var card = new Card
+            {
+
+            };
+        }
+    }
+}
