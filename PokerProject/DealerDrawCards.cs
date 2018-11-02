eadc7b8b33ccecb56c2095788eb6ac93df2d5068
Swathi Chakkilam <swathichakkilam@gmail.com>
2018-11-02 01:08:53 -0700

    created a new class DealerDrawCards which is lika a design pattern class that draws the cards from the Deck and distributes to each player

---
+++ PokerProject/DealerDrawCards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerProject
+{
+   static class DealerDrawCards
+    {
+public static void pullcards()
+        {
+
+        }
+    }
+}
