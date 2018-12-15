6267b3948e660ea51a968df78822d9cfe626c80c
Swathi Chakkilam <swathichakkilam@gmail.com>
2018-12-15 15:57:20 -0800

    Changes have been made in DeckofCard class

--- PokerProject/Card.cs
+++ PokerProject/Card.cs
@@ -4,36 +4,23 @@ using System.Text;
 
 namespace PokerProject
 {
+    public enum Suit
+    {
+        HEARTS,
+        SPADES,
+        DIAMONDS,
+        CLUBS
+    }
+
+    public enum CardValue
+    {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN,
+        EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
     class Card
     {
-        public enum Suit
-        {
-            HEARTS,
-            SPADES,
-            DIAMONDS,
-            CLUBS
-        }
-        public enum CardValue
-        {
-            one,two,three,four,five,six,seven,
-            eight,nine,ten,Jack,Queen,King,Ace
-        }
-        #region Card Properties
-        /// <summary>
-        /// CurrSuitVal is a property of type Suit which is an enum 
-        /// where CurrSuitVal is a way to retrieve the current suit value and store it in CurrSuitVal
-        /// </summary>
-        public Suit CurrSuitVal { get; set; }
-
-        /// <summary>
-        /// CurrentCardVal is a property of type CardValue which is an enum
-        /// where CurrentCardVal holds the current card value at that moment 
-        /// </summary>
-        //used camel casing for property names for readability 
-       public CardValue CurrentCardVal { get; set; }
-        #endregion
-
-
-
+        public Suit suit { get; set; }
+        private CardValue value { get; set; }
     }
 }
---
+++ PokerProject/DeckOfCards.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerProject
+{
+    class DeckOfCards : Card
+    {
+        const int Numofcards = 52;
+        /// <summary>
+        /// deckofcards is an array of type Card where Card is a reference type so both suit value
+        /// and card number or cardvalue exist within each index of the array of type Card[]
+        /// </summary>
+        private Card[] deckofcards;
+
+        public DeckOfCards()
+            {
+            /*allocating memory space via new keyword in memory 
+            for the private array deck and accessing it via Constructor*/
+            this.deckofcards = new Card[Numofcards];
+            }
+    }
+}
